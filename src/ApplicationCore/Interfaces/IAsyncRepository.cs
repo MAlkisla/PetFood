4051966c04b1cfdfcde4a5d309db2bf35a55a65f
Meriç ALKIŞLA <meric.alkisla@outlook.com.tr>
2021-02-09 11:06:08 +0300

    EFRerpository

--- src/ApplicationCore/Interfaces/IAsyncRepository.cs
+++ src/ApplicationCore/Interfaces/IAsyncRepository.cs
@@ -8,18 +8,17 @@ using System.Threading.Tasks;
 
 namespace ApplicationCore.Interfaces
 {
-    interface IAsyncRepository<T> where T : BaseEntity 
+    public interface IAsyncRepository<T> where T : BaseEntity 
     {
         Task<T> GetByIdAsync(int Id);
-
         Task<List<T>> ListAllAsync();
         Task<List<T>> ListAsync(ISpecification<T> spec);
         Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
         Task<int> CountAsync(ISpecification<T> spec);
-        Task<T> First(ISpecification<T> spec);
-        Task<T> FirstOrDefault(ISpecification<T> spec);
+        Task<T> FirstAsync(ISpecification<T> spec);
+        Task<T> FirstOrDefaultAsync(ISpecification<T> spec);
 
 
 
