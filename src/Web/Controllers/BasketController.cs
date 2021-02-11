83fc07462594fa741089e484e26294d2a7c53f6c
Meriç ALKIŞLA <meric.alkisla@outlook.com.tr>
2021-02-11 13:17:33 +0300

    basket service

---
+++ src/Web/Controllers/BasketController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult AddToBasket(ProductViewModel productDetails)
+        {
+
+            return Json(new { BasketItemCount = 0});
+        }
+    }
+}
